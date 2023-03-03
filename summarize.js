b3e5551b108135b093424924ebd770fbe20c7a26
Vishakh Prakash <vishakhpro2002@gmail.com>
2023-03-03 19:34:31 +0530

    summarize.js created

---
+++ summarize.js
@@ -0,0 +1,12 @@
+var minibox = document.createElement("div")
+var par = document.textContent("you are on fire")
+minibox.appendChild(par)
+document.body.appendChild(minibox)
+
+
+minibox.style.position = "absolute"
+minibox.style.top= "50px"
+minibox.style.left= "200px"
+minibox.style.border= "5px solid red"
+
+
